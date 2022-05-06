1e06e058486214fc531f45e41f07886bff1a9dd1
NzrHsp <nazar.hushpit@gmail.com>
2022-05-06 19:59:04 +0300

    Split card to components

--- src/index.js
+++ src/index.js
@@ -2,13 +2,27 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail text="Hola!" />
-      <CommentDetail text="Bonjour!" />
-      <CommentDetail text="Hello!" />
+      <ApprovalCard>
+        <div>
+          <span>Are you sure?</span>
+          <br />
+          <strong>Or you arent?</strong>
+        </div>
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail text="Hola!" />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail text="Bonjour!" />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail text="Hello!" />
+      </ApprovalCard>
     </div>
   );
 };
